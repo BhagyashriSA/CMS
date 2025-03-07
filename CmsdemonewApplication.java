bdb93f4431210a0c01887ebe927b9e3287940632
BhagyashriSA <92725573+BhagyashriSA@users.noreply.github.com>
2025-03-07 18:09:54 +0530

    Add files via upload

---
+++ CmsdemonewApplication.java
@@ -0,0 +1,14 @@
+package com.in.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsdemonewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsdemonewApplication.class, args);
+	System.out.println("hello and welcome");
+	}
+
+}
