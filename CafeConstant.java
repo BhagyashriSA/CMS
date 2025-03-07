b856efa816cf52ef6379367f6334a6612a75b177
BhagyashriSA <92725573+BhagyashriSA@users.noreply.github.com>
2025-03-07 18:01:46 +0530

    Add files via upload

---
+++ CafeConstant.java
@@ -0,0 +1,9 @@
+package com.in.cafe.contents;
+
+public class CafeConstant {
+	
+	public static final String SOMETHING_WENT_WROMG = "Something went wrong.";
+	public static final String INVALID_DATA = "Invalid Data.";
+
+
+}
